07b468b10f7d313605486e7705df9c1652fa821f
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-11-15 22:03:44 -0300

    :sparkles: feat(backend): create Transaction model

---
+++ app/backend/src/database/models/Transactions.ts
@@ -0,0 +1,47 @@
+import * as Sequelize from 'sequelize';
+import db from '.';
+
+interface ITransaction {
+  id: number;
+  debitedAccountId: number;
+  creditedAccountId: number;
+  value: number;
+  createdAt: Date;
+}
+
+type ITransactionCreation = Omit<ITransaction, 'id' | 'createdAt'>;
+
+type ITransactionReturned = ITransaction;
+
+class Transaction extends Sequelize.Model<ITransaction, ITransactionCreation> {
+  declare id: number;
+  declare debitedAccountId: number;
+  declare creditedAccountId: number;
+  declare value: number;
+  declare createdAt: Date;
+}
+
+Transaction.init(
+  {
+    id: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    debitedAccountId: Sequelize.INTEGER,
+    creditedAccountId: Sequelize.INTEGER,
+    value: Sequelize.FLOAT,
+    createdAt: Sequelize.DATE,
+  },
+  {
+    sequelize: db,
+    tableName: 'Transactions',
+    timestamps: true,
+    updatedAt: false,
+    underscored: false,
+  }
+);
+
+export default Transaction;
+export { ITransaction, ITransactionCreation, ITransactionReturned };
