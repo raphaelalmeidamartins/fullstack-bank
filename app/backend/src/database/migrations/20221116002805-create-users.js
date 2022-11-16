c61ec209ca8e065a7d8e6356c47ed7bf4afd2e97
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-11-15 22:02:41 -0300

    :sparkles: feat(backend): create Users migration

---
+++ app/backend/src/database/migrations/20221116002805-create-users.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      username: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      accountId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Accounts',
+          key: 'id',
+        }
+      },
+    });
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.dropTable('Users');
+  }
+};
