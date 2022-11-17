ced679218ec562987168497da4b3e465adb23acc
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-11-16 22:33:33 -0300

    :sparkles: feat(backend): create Token strategy

---
+++ app/backend/src/services/strategies/token/Token.ts
@@ -0,0 +1,32 @@
+import * as jwt from 'jsonwebtoken';
+import UnauthorizedError from '../../../utils/errors/UnauthorizedError';
+import ITokenPayload from './types/ITokenPayload';
+
+class Token {
+  constructor(private tokenModule = jwt) {}
+
+  static async generate(payload: ITokenPayload): Promise<string> {
+    const token = jwt.sign(
+      payload,
+      String(process.env.JWT_SECRET),
+      {
+        expiresIn: '1d',
+      },
+    );
+    return token;
+  }
+
+  static async validate(
+    authorization: string | undefined,
+  ): Promise<ITokenPayload> {
+    if (!authorization) throw new UnauthorizedError('Token não encontrado');
+    const token = authorization;
+    const payload = jwt.verify(
+      token,
+      String(process.env.JWT_SECRET),
+    );
+    return payload as ITokenPayload;
+  }
+}
+
+export default Token;
