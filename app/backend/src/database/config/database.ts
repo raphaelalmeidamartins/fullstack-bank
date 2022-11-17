4c82834cbe48cee81dd78b59872872adec3e0d65
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-11-17 01:08:32 -0300

    :recycle: refactor(backend): change database name

--- app/backend/src/database/config/database.ts
+++ app/backend/src/database/config/database.ts
@@ -4,7 +4,7 @@ import { Options } from 'sequelize';
 const config: Options = {
   username: process.env.DB_USER || 'root',
   password: process.env.DB_PASS || '123456',
-  database: 'TODO_LIST',
+  database: 'BANK',
   host: process.env.DB_HOST || 'localhost',
   port: Number(process.env.DB_PORT) || 3002,
   dialect: 'mysql',
