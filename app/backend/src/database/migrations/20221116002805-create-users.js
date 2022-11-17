f09ed8b627fcfec3ce92d6e71a53c32aef424227
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-11-17 01:19:19 -0300

    :bug: fix(backend): add unique constraint
    to the username column in the Users table

--- app/backend/src/database/migrations/20221116002805-create-users.js
+++ app/backend/src/database/migrations/20221116002805-create-users.js
@@ -12,6 +12,7 @@ module.exports = {
       username: {
         type: Sequelize.STRING,
         allowNull: false,
+        unique: true,
       },
       password: {
         type: Sequelize.STRING,
