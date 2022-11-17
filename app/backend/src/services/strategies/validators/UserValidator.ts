a41fb15a8f88383e9b8ae64b251a86506b5f7145
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-11-16 22:33:02 -0300

    :sparkles: feat(backend): create UserValidator strategy

---
+++ app/backend/src/services/strategies/validators/UserValidator.ts
@@ -0,0 +1,49 @@
+import * as Joi from 'joi';
+import { IUserLogin, IUserRegister } from '../../../database/models/User';
+import generateValidatorFunction from './helpers/generateValidatorFunction';
+import IValidatorFunction from './types/IValidatorFunction';
+
+const REQUIRED_MSG = 'Todos os campos precisam ser preenchidos';
+const USERNAME_LENGTH_MSG =
+  'O nome de usuário precisa ter no mínimo 3 caracteres';
+const PASSWORD_PATTERN_MSG =
+  'A senha precisa ter pelo menos 8 caracteres, um número e uma letra maiúscula';
+
+class UserValidator {
+  static login = generateValidatorFunction(
+    Joi.object({
+      username: Joi.string().required().messages({
+        'string.empty': REQUIRED_MSG,
+        'string.base': REQUIRED_MSG,
+        'any.required': REQUIRED_MSG,
+      }),
+      password: Joi.string().required().messages({
+        'string.empty': REQUIRED_MSG,
+        'string.base': REQUIRED_MSG,
+        'any.required': REQUIRED_MSG,
+      }),
+    })
+  ) as IValidatorFunction<IUserLogin>;
+
+  static register = generateValidatorFunction(
+    Joi.object({
+      username: Joi.string().min(3).required().messages({
+        'any.required': REQUIRED_MSG,
+        'string.empty': REQUIRED_MSG,
+        'string.base': REQUIRED_MSG,
+        'string.min': USERNAME_LENGTH_MSG,
+      }),
+      password: Joi.string()
+        .regex(/^(?=.*\d)(?=.*[A-Z]).{8,}$/)
+        .required()
+        .messages({
+          'any.required': REQUIRED_MSG,
+          'string.empty': REQUIRED_MSG,
+          'string.base': REQUIRED_MSG,
+          'string.regex': PASSWORD_PATTERN_MSG,
+        }),
+    })
+  ) as IValidatorFunction<IUserRegister>;
+}
+
+export default UserValidator;
