9fe2f0f5341b316422fb5c4219fc110ef77dfd9b
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-11-18 19:12:40 -0300

    :sparkles: feat(backend): create TransactionValidator class

---
+++ app/backend/src/services/strategies/validators/TransactionValidator.ts
@@ -0,0 +1,27 @@
+import * as Joi from 'joi';
+import { ITransactionRegister } from '../../../database/models/Transactions';
+import generateValidatorFunction from './helpers/generateValidatorFunction';
+import IValidatorFunction from './types/IValidatorFunction';
+
+const REQUIRED_MSG = 'Todos os campos precisam ser preenchidos.';
+const VALUE_MSG =
+  'O valor da transação precisa ser um valor numérico positivo';
+
+class TransactionValidator {
+  static register = generateValidatorFunction(
+    Joi.object({
+      username: Joi.string().required().messages({
+        'any.required': REQUIRED_MSG,
+        'string.empty': REQUIRED_MSG,
+        'string.base': REQUIRED_MSG,
+      }),
+      value: Joi.number().positive().required().messages({
+        'any.required': REQUIRED_MSG,
+        'number.base': VALUE_MSG,
+        'number.positive': VALUE_MSG,
+      }),
+    })
+  ) as IValidatorFunction<ITransactionRegister>;
+}
+
+export default TransactionValidator;
