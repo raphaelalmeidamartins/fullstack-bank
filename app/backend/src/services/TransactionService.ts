be668ba3afb4484afaabf859def9c704efd4d7de
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-11-18 19:13:14 -0300

    :sparkles: feat(backend): create TransactionService class

---
+++ app/backend/src/services/TransactionService.ts
@@ -0,0 +1,114 @@
+import db from '../database/models';
+import Account from '../database/models/Account';
+import Transaction, {
+  ITransaction,
+  ITransactionRegister
+} from '../database/models/Transactions';
+import User from '../database/models/User';
+import NotFoundError from '../utils/errors/NotFoundError';
+import UnprocessableEntityError from '../utils/errors/UnprocessableEntityError ';
+import Token from './strategies/token/Token';
+import TransactionValidator from './strategies/validators/TransactionValidator';
+
+class TransactionService {
+  private _repository = Transaction;
+  private _userRepository = User;
+  private _accountRepository = Account;
+  private _validator = TransactionValidator;
+  private _tokenModule = Token;
+
+  async register(
+    authorization: string | undefined,
+    body: ITransactionRegister
+  ): Promise<void> {
+    this._validator.register(body);
+
+    const { id } = await this._tokenModule.validate(authorization);
+
+    const debitedUser = await this._userRepository.findByPk(id);
+    const debitedAccount = await this._accountRepository.findByPk(
+      debitedUser?.accountId
+    );
+    const creditedUser = await this._userRepository.findOne({
+      where: { username: body.username },
+    });
+    const creditedAccount = await this._accountRepository.findByPk(
+      creditedUser?.accountId
+    );
+
+    if (!debitedAccount || !creditedAccount)
+      throw new NotFoundError(
+        'Dados de transação inválidos, a transação não foi realizada.'
+      );
+
+    if (debitedAccount.balance < body.value) {
+      throw new UnprocessableEntityError(
+        'Você não tem saldo suficiente para realizar esta transação.'
+      );
+    }
+
+    if (debitedAccount.id === creditedAccount.id) {
+      throw new UnprocessableEntityError(
+        'Não é possível fazer uma transação para a mesma conta.'
+      );
+    }
+
+    const transaction = await db.transaction();
+
+    try {
+      await this._repository.create(
+        {
+          debitedAccountId: id,
+          creditedAccountId: creditedAccount.id,
+          value: body.value,
+        },
+        { transaction }
+      );
+
+      await debitedAccount.update(
+        { balance: debitedAccount.balance - body.value },
+        { transaction }
+      );
+      await creditedAccount.update(
+        { balance: creditedAccount.balance + body.value },
+        { transaction }
+      );
+
+      transaction.commit();
+    } catch (error) {
+      transaction.rollback();
+      throw error;
+    }
+  }
+
+  async list(
+    authorization: string | undefined,
+    type: string | undefined
+  ): Promise<ITransaction[]> {
+    const { id } = await this._tokenModule.validate(authorization);
+
+    let transactions: ITransaction[];
+
+    switch (type) {
+      case 'cashout':
+        transactions = await this._repository.findAll({
+          where: { debitedAccountId: id },
+        });
+        break;
+
+      case 'cashin':
+        transactions = await this._repository.findAll({
+          where: { creditedAccountId: id },
+        });
+        break;
+
+      default:
+        transactions = await this._repository.findAll();
+        break;
+    }
+
+    return transactions;
+  }
+}
+
+export default TransactionService;
