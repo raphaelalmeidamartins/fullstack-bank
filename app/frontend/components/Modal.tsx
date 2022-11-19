cbe1447140350604705c069fe3989755ed6828fa
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-11-19 16:17:44 -0300

    :recycle: refactor(frontend): move the modal to a separated component

---
+++ app/frontend/components/Modal.tsx
@@ -0,0 +1,37 @@
+import styles from '../styles/components/Modal.module.css';
+
+export default function Modal({
+  display,
+  message,
+  buttonMessage,
+  handleClick,
+}: {
+  display: boolean;
+  message: string;
+  buttonMessage: string;
+  handleClick: Function;
+}) {
+  return (
+    <div>
+      <div
+        className={`${styles.modal__overlay} ${
+          display ? styles['modal--display'] : styles['modal--hidden']
+        }`}
+      />
+      <div
+        className={`${styles.modal} ${
+          display ? styles['modal--display'] : styles['modal--hidden']
+        }`}
+      >
+        <p>{message}</p>
+        <button
+          className={styles.modal__button}
+          type="button"
+          onClick={() => handleClick()}
+        >
+          {buttonMessage}
+        </button>
+      </div>
+    </div>
+  );
+}
