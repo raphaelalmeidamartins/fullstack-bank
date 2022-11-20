cae29d743cfda2b9dba03b93c035e660e6dbb298
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-11-20 20:39:07 -0300

    :wrench: chore: change nyc settings to ignore some directories and files

--- app/backend/nyc.config.js
+++ app/backend/nyc.config.js
@@ -2,10 +2,12 @@ module.exports = {
   all: true,
   extends: "@istanbuljs/nyc-config-typescript",
   exclude: [
-    'src/tests',
+    'src/__tests__',
     'src/database/config',
     'src/database/migrations',
-    'src/database/seeders'
+    'src/database/seeders',
+    'src/app.ts',
+    'src/server.ts'
   ],
   include: ['src/**/*.ts']
 };
