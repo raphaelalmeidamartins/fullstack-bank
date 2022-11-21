871dea7628076255ad33d33fab944126e29199b5
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-11-21 01:17:04 -0300

    :wrench: chore(E2E): add configuration to ignore uncaught promise exceptions
    The tests were failing in Firefox because of that

--- app/backend/cypress/support/e2e.ts
+++ app/backend/cypress/support/e2e.ts
@@ -16,5 +16,9 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 
+Cypress.on('uncaught:exception', (err, runnable) => {
+  return false
+})
+
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
